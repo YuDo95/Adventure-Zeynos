9aa7fd4378c3710228a65ad0e8240b76c2d49c50
Yudo95 <yudo0001@stud.kea.dk>
2024-10-02 09:34:00 +0200

    test

---
+++ src/main/java/dk/kea/adventurezeynos/AdventureZeynosApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.adventurezeynos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureZeynosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureZeynosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/adventurezeynos/AdventureZeynosApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.adventurezeynos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureZeynosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
